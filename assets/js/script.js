2ddc9e90f064b54872f816de962461b15a3c2f73
unknown <cuitlahuacjr@gmail.com>
2022-04-04 19:16:19 -0500

    Created checkTimeBlock function to set correct class(past,present,future)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 //Set today's date
 var currentDayElement = $('#currentDay');
 currentDayElement.text(moment().format('dddd, MMMM Do'));
+
+//Function to define if the time block is in the past, present or future.
+function checkTimeBlock() {
+    let currentTime = moment().hour();
+
+    $('.time-block').each(function () {
+       let blockHour = $(this).attr('id');
+       let textArea = $(this).find('textarea')
+       console.log(textArea);
+
+       if (blockHour < currentTime)
+           textArea.addClass('past');
+       else if (blockHour > currentTime)
+           textArea.addClass('future');
+       else if (blockHour == currentTime)
+           textArea.addClass('present')
+    });
+}
+
+checkTimeBlock();
